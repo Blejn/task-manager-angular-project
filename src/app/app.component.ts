721effa1368ff80e733cd257ffab38f2b5fc8711
Blejn <blejntypie@gmail.com>
2021-11-26 14:13:46 +0100

    Interpolacja, Pipes oraz Question marki
    Interpolacja, Pipes oraz Question marki

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'task-manager';
+
+config: { [key:string]: string} | null = null;
+
+constructor() {
+  setTimeout( () => {
+  this.config = {
+  title:'Lista zadań',
+  footer: 'Lista zadań zbudowana w Angularze',
+  date: new Date().toDateString()
+  };
+},500);
+
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { registerLocaleData } from '@angular/common';
+import localePl from '@angular/common/locales/pl'
 
+
+registerLocaleData(localePl)
 @NgModule({
   declarations: [
     AppComponent
@@ -10,7 +14,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule 
   ],
-  providers: [], //information about service
+  providers: [ {provide: LOCALE_ID, useValue:'pl-PL'}], //information about service
   bootstrap: [AppComponent]  //start component
 })
 export class AppModule { }
